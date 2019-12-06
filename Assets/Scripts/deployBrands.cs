41a6ac8ad965acc98d3e72750b20795a15b79efb
nikhilnj96 <nikhilnj96@gmail.com>
2019-12-06 23:08:01 +0530

    UI working after build

--- Assets/Scripts/deployBrands.cs
+++ Assets/Scripts/deployBrands.cs
@@ -4,44 +4,60 @@ using UnityEngine;
 
 public class deployBrands : MonoBehaviour
 {
-    public GameObject brandPrefab;
-    public GameObject brandPrefab2;
-    public GameObject brandPrefab3;
-    public GameObject bomb;
+    public GameObject brandPrefab1, brandPrefab2, brandPrefab3, bomb;
 
+    GameObject a;
+    List<GameObject> brands = new List<GameObject>();
+
+    int test = 0, testPosition = 30, checkUserPreference = 0;
     float respawnTime = 1f;
     Vector2 screenBounds;
 
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-20, Screen.height));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-40, Screen.height));
+        brands.Add(brandPrefab1);
+        brands.Add(brandPrefab2);
+        brands.Add(brandPrefab3);
+        brands.Add(bomb);
+
         StartCoroutine(wave());
     }
 
     void create()
     {
         int i = Random.Range(0, 10) % 4;
-        if (i == 0)
+
+        if (checkUserPreference == 0)
         {
-            GameObject a = Instantiate(brandPrefab) as GameObject;
+            int temp = Random.Range(1, 10);
+            if (temp >= 7)
+            {
+                checkUserPreference = 1;
+                Debug.Log("trap coming");
+            }
+        }
+        if (checkUserPreference == 0)
+        {
+            a = Instantiate(brands[i]) as GameObject;
             a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
         }
-        else if (i==1)
+        else if (checkUserPreference == 1)
         {
-            GameObject a = Instantiate(brandPrefab2) as GameObject;
+            a = Instantiate(brands[i]) as GameObject;
             a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
-        } 
-        else if (i==2)
-        {
-            GameObject a = Instantiate(brandPrefab3) as GameObject;
-            a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
+            checkUserPreference++;
         }
-        else
+        else if (checkUserPreference == 2)
         {
-            GameObject a = Instantiate(bomb) as GameObject;
-            a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
+            Debug.Log("wohoo");
+            a = Instantiate(brands[i]) as GameObject;
+            a.transform.position = new Vector2(testPosition, screenBounds.y * 2);
+
+            a = Instantiate(brands[i]) as GameObject;
+            a.transform.position = new Vector2(-testPosition, screenBounds.y * 2);
+            checkUserPreference = 0;
         }
-        
     }
 
     IEnumerator wave()
